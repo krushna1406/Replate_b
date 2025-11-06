415992216f012d646c76c03d9781251be1f71e2f
krushna1406 <rautkrishna931@gmail.com>
2025-11-06 21:44:27 +0530

    little case changes

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 // Optional: fallback to index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "Public", "index.html"));
 });
 
 const fs = require("fs");
